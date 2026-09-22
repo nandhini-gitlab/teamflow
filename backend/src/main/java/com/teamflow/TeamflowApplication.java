96bf518283926831f1d4a9fa8b0232be127fdf48
Manivel S <manivel.s@indium.tech>
2026-09-22 18:21:48 +0530

    backend dependencies added

---
+++ backend/src/main/java/com/teamflow/TeamflowApplication.java
@@ -0,0 +1,13 @@
+package com.teamflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamflowApplication.class, args);
+	}
+
+}
